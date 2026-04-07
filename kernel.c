d519a3c7b961b13ea8f1cafc3ab7c24597852222
niqqsucks <draovamshi@gmail.com>
2026-04-08 01:05:21 +0530

    Initialize IDT and IRQ in kernel

--- kernel.c
+++ kernel.c
@@ -1,16 +1,20 @@
 #include "drivers/screen.h"
 #include "cpu/idt.h"
+#include "cpu/irq.h"
 
 void kernel_main()
 {
     clear_screen();
 
     print_string("ZenithOS v1.0\n");
-    print_string("Loading IDT...\n");
 
+    print_string("Loading IDT...\n");
     idt_init();
 
-    print_string("IDT Loaded\n");
+    print_string("Remapping PIC...\n");
+    irq_init();
+
+    print_string("IRQ Ready\n");
 
     while(1);
 }
