3c66060911245ecf9759898b983d2bb65597ab7a
niqqsucks <draovamshi@gmail.com>
2026-04-08 00:53:51 +0530

    Update kernel implementation

--- kernel.c
+++ kernel.c
@@ -1,11 +1,16 @@
 #include "drivers/screen.h"
+#include "cpu/idt.h"
 
-void kernel_main() {
-
+void kernel_main()
+{
     clear_screen();
 
     print_string("ZenithOS v1.0\n");
-    print_string("Boot Successful\n\n");
+    print_string("Loading IDT...\n");
 
-    print_string("Welcome to ZenithOS Kernel");
+    idt_init();
+
+    print_string("IDT Loaded\n");
+
+    while(1);
 }
